1742b8cb6a0c1f51a2cf10f751c7ea3fc0bf6591
beesou777 <sbeesou@gmail.com>
2024-09-29 18:01:14 +0545

    feat : email design

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -3,7 +3,7 @@ import nodemailer from 'nodemailer';
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { email, subject, message } = body;
+    const {name, email, subject, message } = body;
     
     const transporter = nodemailer.createTransport({
       host: 'smtp.zoho.com',
@@ -27,8 +27,61 @@ export async function POST(req: Request) {
       from: process.env.ZOHO_USER,
       to: email,
       subject: 'Thank you for contacting us!',
-      text: `Dear ${email},\n\nThank you for reaching out! We will get back to you soon.\n\nBest regards,\nTravelers Himalaya`,
-      html: `<p>Dear <strong>${email}</strong>,</p><p>Thank you for reaching out! We will get back to you soon.</p><p>Best regards,<br/>Travelers Himalaya</p>`,
+      html: `
+      
+      <table style="text-align: center;max-width: 400px;font-family: sans-serif;margin:auto">
+      <tr>
+        <td>
+          <img
+            height="250"
+            width="400"
+            src="https://postimg.cc/3kn7DwCV"
+            alt="techunter"
+          />
+        </td>
+      </tr>
+      <tr>
+        <td style="padding: 10px 0;">
+          <h2 style="margin: 0">Dear ,c  <strong>${name}</strong></h2>
+        </td>
+      </tr>
+      <tr>
+        <td style="padding: 10px 0;">
+          Thank you for your email! We appreciate you taking the time to reach out to us. Our team will contact you soon.
+        </td>
+      </tr>
+      <tr>
+        <td style="padding: 10px 0;">
+          While you're here, we invite you to check out our latest blog post on our website. It’s packed with insights
+          and information that we believe you will find interesting and helpful.
+        </td>
+      </tr>
+      <tr>
+        <td style="padding: 10px 0;">
+          Feel free to explore, and if you have any questions or need further assistance, don’t hesitate to let us know!
+        </td>
+      </tr>
+      <tr>
+        <td style="text-align: center; padding: 20px 0">
+          <a
+            style="
+              margin: auto;
+              padding: 9px 10px;
+              border-radius: 6px;
+              border: none;
+              background: black;
+              color: whitesmoke;
+              width: fit-content;
+              font-weight: bold;
+              text-decoration: none;
+            "
+            href="https://www.techunterhub.com/"
+            >Check out latest blog</a
+          >
+        </td>
+      </tr>
+    </table>
+      `,
   };
 
   
