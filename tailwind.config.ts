be9ba99e907f8434e8f79654906aa96a49b428b5
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-10-04 18:26:31 +0545

    build: stage-build prod-build

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        roboto: "Roboto, sans-serif",
+      },
     },
   },
   plugins: [],
