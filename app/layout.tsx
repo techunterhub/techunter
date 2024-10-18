547a9c23a3b40cd067347c6c9a7d29b12487766f
Pramod Laha <pramodlaha.pm.08@gmail.com>
2024-10-18 19:15:21 +0545

    feat: clarity code from microsoft

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from "../store/provider";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer/Footer";
 import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Techunter",
@@ -18,6 +19,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "okkpbprvnt");
+            `,
+          }}
+        />
+      </Head>
       <body className={`antialiased`}>
         <Providers>
           <Navbar />
